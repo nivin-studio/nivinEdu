21d89eb0d0b1fd1f5c7f419ae40c8d3201565b78
ving <ving@nivin.cn>
2020-08-23 12:44:18 +0800

    特性：主页重定向

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,8 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('h5.index');
 });
