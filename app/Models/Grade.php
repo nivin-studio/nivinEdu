48b1e7e17dea9d4409433f47575efa8a1c86a69c
ving <ving@nivin.cn>
2020-08-24 16:22:04 +0800

    特性：添加ide_helper

--- app/Models/Grade.php
+++ app/Models/Grade.php
@@ -5,6 +5,48 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * App\Models\Grade
+ *
+ * @property int $id
+ * @property int $school_id 学校
+ * @property string $xh 学号
+ * @property string $xn 学年
+ * @property int $xq 学期
+ * @property string $kh 课号
+ * @property string $km 课名
+ * @property string $kx 课型
+ * @property string $cj 成绩
+ * @property string $xf 学分
+ * @property string $jd 绩点
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string|null $deleted_at
+ * @property-read \App\Models\School $school
+ * @method static bool|null forceDelete()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Grade newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Grade newQuery()
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Grade onlyTrashed()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Grade query()
+ * @method static bool|null restore()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Grade whereCj($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Grade whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Grade whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Grade whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Grade whereJd($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Grade whereKh($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Grade whereKm($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Grade whereKx($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Grade whereSchoolId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Grade whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Grade whereXf($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Grade whereXh($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Grade whereXn($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Grade whereXq($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Grade withTrashed()
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Grade withoutTrashed()
+ * @mixin \Eloquent
+ */
 class Grade extends Model
 {
     use SoftDeletes;
--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,53 @@ namespace App\Models;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+/**
+ * App\Models\User
+ *
+ * @property int $id
+ * @property int $school_id 学校
+ * @property string $xh 学号
+ * @property string $mm 密码
+ * @property string $xm 姓名
+ * @property string $sf 身份证
+ * @property int $xb 性别 0:未知,1:男生,2:女生
+ * @property string $sr 出生日期
+ * @property string $mz 民族
+ * @property string $xl 学历
+ * @property string $xy 学院
+ * @property string $zy 专业
+ * @property string $bj 班级
+ * @property string $xz 学制
+ * @property string $nj 年级
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @property-read int|null $notifications_count
+ * @property-read \App\Models\School $school
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereBj($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereMm($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereMz($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereNj($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereSchoolId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereSf($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereSr($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereXb($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereXh($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereXl($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereXm($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereXy($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereXz($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereZy($value)
+ * @mixin \Eloquent
+ */
 class User extends Authenticatable
 {
     use Notifiable;
