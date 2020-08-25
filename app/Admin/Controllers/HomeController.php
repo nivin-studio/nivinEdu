808e122a5603e45da43a537b14d4ab9662498385
ving <ving@nivin.cn>
2020-08-25 22:42:44 +0800

    特性：修改主页数据展示

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -2,8 +2,11 @@
 
 namespace App\Admin\Controllers;
 
-use App\Admin\Metrics\UsersLine;
+use App\Admin\Metrics\NumCard;
 use App\Http\Controllers\Controller;
+use App\Models\Grade;
+use App\Models\School;
+use App\Models\User;
 use Dcat\Admin\Layout\Column;
 use Dcat\Admin\Layout\Content;
 use Dcat\Admin\Layout\Row;
@@ -15,21 +18,27 @@ class HomeController extends Controller
         return $content
             ->header('应用概况')
             ->body(function (Row $row) {
-                $row->column(3, function (Column $column) {
-                    $column->row(new UsersLine());
+                $row->column(4, function (Column $column) {
+                    $numCard = new NumCard();
+                    $numCard->title('学校数');
+                    $numCard->setNum(School::count());
+                    $column->row($numCard);
                 });
 
-                $row->column(3, function (Column $column) {
-                    $column->row(new UsersLine());
+                $row->column(4, function (Column $column) {
+                    $numCard = new NumCard();
+                    $numCard->title('学生数');
+                    $numCard->setNum(User::count());
+                    $column->row($numCard);
                 });
 
-                $row->column(3, function (Column $column) {
-                    $column->row(new UsersLine());
+                $row->column(4, function (Column $column) {
+                    $numCard = new NumCard();
+                    $numCard->title('成绩数');
+                    $numCard->setNum(Grade::count());
+                    $column->row($numCard);
                 });
 
-                $row->column(3, function (Column $column) {
-                    $column->row(new UsersLine());
-                });
             });
     }
 }
